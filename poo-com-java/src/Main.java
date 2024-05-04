35847153b04aede09891845125fed14757db33af
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2024-05-03 23:17:57 -0300

    class & object

---
+++ poo-com-java/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Phone phone = new Phone();
+
+        phone.diskSpace = 256;
+        phone.name = "iPhone 12";
+        phone.operationalSystem = "iOS";
+        phone.screenSize = 5.1F;
+    }
+}
---
+++ poo-com-java/src/Phone.java
@@ -0,0 +1,6 @@
+public class Phone {
+    String name;
+    String operationalSystem;
+    int diskSpace = 256;
+    float screenSize = 256;
+}
